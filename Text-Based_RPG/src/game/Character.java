e82581a952888e1bc9ae5a18d6c12987607e932a
AntiCavity <71098766+AntiCavity@users.noreply.github.com>
2022-11-14 19:39:38 -0600

    transfered character classes
    I transfered the classes I just made to the right class

--- Text-Based_RPG/src/game/Character.java
+++ Text-Based_RPG/src/game/Character.java
@@ -34,8 +34,18 @@ public class Character {
 			+ "fog appearing from the base of the blade. It looks familiar, but you "
 			+ "can't quite remember where you saw it from."};
 	
-	String[] archerArmorDes = {};
-	String[] archerWeapon = {};
+	String[] archerArmorDes = {"Ranger's Cloak", "This signature cloak identifies one as a King's Ranger, handpicked to serve the"
+			+ "King as wardens of the various provinces that make up the Kingdom of Araluen"
+			+ "The Ranger's Cloak sports a motley pattern of various colors of the forests found in the Kingdom."
+			+ "Whoever wears this cloak has gone through rigorous tests to be selected as one of the best archers in the Kingdom."};
+	String[] archerWeaponDes = {"Longbow", "This bow has a draw weight of 185 pounts, meaning only very strong individuals can use"
+			+ "this weapon effectively, along with the jet black bodkin tipped arrows, only the most sturdy of foes can shrug off an arrow"
+			+ "from a Longbow"};
+	
+	String[] warriorArmorDes = {"Warrior Rags", "These garments are not fit for wearing, much less for battle, at the"
+			+ " very least you can move swiftly in them."};
+	String[] warriorWeaponDes = {"Twin Scimitars", "These swords, come with a long curve, making them unsuited to thrusting."
+			+ "But because of the long curve, they excel at slicing through flesh and bone."};
 	
 	// Add other class Armor and Weapons here
 		//TODO
@@ -164,6 +174,98 @@ public class Character {
 					this.attackSkills = rogueSkillSet;
 					// Need more skill IDEAS!
 						//TODO
+	}
+	
+	public void archerClassSetUp() {
+		Armor rangerArmor = new Armor();
+			rangerArmor.name = archerArmorDes[0];
+			rangerArmor.itemDescription = archerArmorDes[1];
+			rangerArmor.damageProtection = 5;
+			int[] resistance = {1,0,0};	//{poisonResistance, bleedResistance, magicResistance}
+			rangerArmor.armorResistance = resistance;
+		addArmor(rangerArmor,1);
+		
+		Weapon Longbow = new Weapon();
+			Longbow.name = archerWeaponDes[0];
+			Longbow.itemDescription = archerWeaponDes[1];
+			Longbow.damage = 20;
+			Longbow.critChance = 0; //TODO I believe that the Longbow should have average crit change
+		addWeapon(Longbow, 1);
+		
+		this.addHealPotion(2);
+		this.addManaPotion(2);
+		
+		this.healthPoints = 60;
+		this.mana = 30;
+		this.CharClass = "Archer";
+		
+		Skills FullDraw = new Skills();	// Special Skill //
+		FullDraw.name = "Full Draw";
+		FullDraw.skillDescription = "You nock an arrow and draw with your full might, exhausting you heavily";
+		FullDraw.damage = 30; // High Damage but also has a High Mana cost, should be used sparingly
+		FullDraw.energyCost = 10; // Should subract from character.mana
+		FullDraw.heal = 0; // No lifesteal in the Archers skillset
+		
+		Skills Shoot = new Skills(); // Basic Skill //
+		Shoot.name = "Shoot";
+		String sd = String.format("You draw and shoot an arrow from your %s", this.weapon);
+		Shoot.skillDescription = sd;
+		Shoot.damage = this.weapon.damage;
+		Shoot.energyCost = 0;
+		
+		HashSet<Skills> archerSkillSet = new HashSet<>();//{slash, backStab};
+		archerSkillSet.add(Shoot);
+		archerSkillSet.add(FullDraw);
+		
+		this.attackSkills = archerSkillSet;
+	}
+	
+	public void warriorClassSetUp() {
+		Armor warriorArmor = new Armor();
+			warriorArmor.name = warriorArmorDes[0];
+			warriorArmor.itemDescription = warriorArmorDes[1];
+			warriorArmor.damageProtection = 1;
+		
+		addArmor(warriorArmor, 1);
+		
+		Weapon twinScimitars = new Weapon();
+			twinScimitars.name = warriorWeaponDes[0];
+			twinScimitars.itemDescription = warriorWeaponDes[1];
+			twinScimitars.damage = 30;
+			twinScimitars.critChance = 0; // These should be high crit too
+		
+		addWeapon(twinScimitars, 1);
+		
+		addHealPotion(2);
+		addManaPotion(2);
+		
+		this.healthPoints = 100;
+		this.mana = 15;
+		this.CharClass = "Warrior";
+		
+		Skills BloodBath = new Skills();
+			BloodBath.name = "Blood Bath";
+			String sd = String.format( "You cut yourself with your %s "
+					+ "and use the resulting adrenaline to unleash a whirlwind of attacks on your enemy", this.weapon);
+			BloodBath.skillDescription = sd;
+			BloodBath.damage = this.weapon.damage + (this.weapon.damage / 2); // getting a weapon upgrade makes this skills damage go up
+			BloodBath.energyCost = 5;
+			// TODO figure out how to do self damage, this should cost around 10-20 health maybe
+			BloodBath.heal = -20; //This might be a neat work around
+			
+		Skills Swing = new Skills();
+		
+			Swing.name = "Swing";
+			Swing.skillDescription = "You take a swing at your enemy, their pain seemingly healing you as you inflict it";
+			Swing.damage = this.weapon.damage;
+			Swing.energyCost = 0;
+			Swing.heal = this.weapon.damage/3;
+			
+			HashSet<Skills> warriorSkillSet = new HashSet<>();//{slash, backStab};
+			warriorSkillSet.add(Swing);
+			warriorSkillSet.add(BloodBath);
+			
+		this.attackSkills = warriorSkillSet;	
 	}
 	
 	public void createNewInventory() {
